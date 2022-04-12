e3bc83c21c76b992dba71bdf5304869ed834bd99
AdverseGecko3 <eric.barrero@hotmail.com>
2022-04-12 17:47:56 +0200

    Initial commit

---
+++ app/src/main/java/com/adverse2048/gecko3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.adverse2048.gecko3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
